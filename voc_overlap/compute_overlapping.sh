3cd8f92b44d1a702bcbe410bd5474e293def7e0c
Javi GG <s.javi.com@gmail.com>
2024-06-05 23:01:01 +0200

    Update compute_overlapping.sh

--- voc_overlap/compute_overlapping.sh
+++ voc_overlap/compute_overlapping.sh
@@ -4,14 +4,6 @@
 #SBATCH --output=slurm_logs/overlapping%j.out
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:1
-#SBATCH --time=01-23:59:59
-#SBATCH --cpus-per-task=20
-#SBATCH --qos acc_bscls
-#SBATCH -N1
-#SBATCH --account bsc88
-##SBATCH --qos=acc_bscls
-
-source /gpfs/projects/bsc88/mt_translation/environments/training_hf_venv/bin/activate
 
 lang=(deu eus fra ita glg spa por eng)
 
@@ -30,16 +22,16 @@ compute_overlapping(){
 }
 
 flores="devtest"
-flores_path="/gpfs/projects/bsc88/mt_translation/data/flores200_dataset/${flores}"
+flores_path="./flores200_dataset/${flores}"
 
 len_voc="32k"
-tokenizer_path="/gpfs/projects/bsc88/mt_translation/Parallel_LLM/training/saved_checkpoints/gemma32_distributed/checkpoint-315000"
+tokenizer_path=""
 compute_overlapping
 
 len_voc="128k"
-tokenizer_path="/gpfs/projects/bsc88/mt_translation/Parallel_LLM/training/saved_checkpoints/gemma128_distributed/checkpoint-320000"
+tokenizer_path=""
 compute_overlapping
 
 len_voc="256k"
-tokenizer_path="/gpfs/projects/bsc88/mt_translation/Parallel_LLM/training/saved_checkpoints/gemma256_distributed/checkpoint-300000"
+tokenizer_path=""
 compute_overlapping
